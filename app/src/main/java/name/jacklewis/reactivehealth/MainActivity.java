b6d12bab14bb847dda834ac2dff7828809a1a09a
Jack <jack@calderadev.com>
2018-06-01 18:06:58 -0700

    Final push of the day

--- app/src/main/java/name/jacklewis/reactivehealth/MainActivity.java
+++ app/src/main/java/name/jacklewis/reactivehealth/MainActivity.java
@@ -4,16 +4,13 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
 import com.jakewharton.rxbinding2.support.design.widget.RxNavigationView;
 import com.jakewharton.rxbinding2.view.RxView;
 
